c39078b7f7613814a677e4c97ec27cfbb4ea7e6a
Moh. Firman Qodry A <firman.qodry@gmail.com>
2021-04-09 09:12:42 +0700

    Day 3

---
+++ 6 Kyu - Who Likes It/solusi.php
@@ -0,0 +1,16 @@
+<?php
+function likes( $names ) {
+  switch(count($names)) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return "$names[0] likes this";
+    case 2:
+      return "$names[0] and $names[1] like this";
+    case 3:
+      return "$names[0], $names[1] and $names[2] like this";
+    default:
+      return sprintf("%s, %s and %d others like this", $names[0], $names[1], count($names)-2);
+  }
+}
+?>
